b69ac0c1a0531423046cc1ac0753b662f271b757
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-06-06 22:57:29 -0300

    Initial version

---
+++ src/main/java/br/espm/invest/wallet/WalletResource.java
@@ -0,0 +1,11 @@
+package br.espm.invest.wallet;
+
+import br.espm.invest.wallet.common.controller.WalletController;
+import br.espm.invest.wallet.common.datatype.Wallet;
+
+public class WalletResource implements WalletController {
+    @Override
+    public Wallet wallet(String id) {
+        return null;
+    }
+}
